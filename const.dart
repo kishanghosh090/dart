fa2a7fe29753c600823964a95c494fe95a44de52
kishanghosh090 <kishanghosh090@gmail.com>
2025-04-28 00:29:41 +0530

    dart added

--- const.dart
+++ const.dart
@@ -1,5 +1,12 @@
 main() {
   const aConstNum = 0;
   const aConstBool = true;
-  
+
+  print('$aConstNum  $aConstBool');
+  print(aConstNum.runtimeType);
+  print(aConstBool.runtimeType);
+
+  var num = null;
+
+  print(num);
 }
---
+++ operators.dart
@@ -0,0 +1,9 @@
+int main() {
+  int num = 20;
+  if (num % 2 == 0) {
+    print("$num is even");
+  } else {
+    print("$num is odd");
+  }
+  return 0;
+}
