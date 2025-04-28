c429c69d7a5ba9d144f7cf4b8cdae15d6bf94dbe
kishanghosh090 <kishanghosh090@gmail.com>
2025-04-29 00:46:54 +0530

    dart added

--- list.dart
+++ list.dart
@@ -1,9 +1,12 @@
 void main() {
   const names = ['John', 'Doe', 'Jane'];
+  const names2 = [...names];
+  print(names2);
   // names.add('Doe');
   print(names);
   print(names[0]);
   print(names[1]);
   print(names[2]);
   // print(names[3]);
+  
 }
